8db4aeb74c54a4b7604bccf505b2d3d86c556710
Dany Laporte <dany_laporte@hotmail.com>
2016-05-11 20:40:40 -0400

    Simplification of the ToStream operator

--- src/iter.rs
+++ src/iter.rs
@@ -3,20 +3,13 @@ use producer::*;
 use std::rc::Rc;
 use stream::*;
 
+/// Represent a stream on an iterator.
 pub struct IterStream<I> {
     iterator: I,
 }
 
-pub struct IterStreamRef<I> {
-    iterator: I,
-}
-
-pub struct IterStreamRefMut<I> {
-    iterator: I,
-}
-
 impl<I, T> Stream<T> for IterStream<I>
-    where I: Iterator<Item = T> + 'static
+    where I: Iterator<Item = T>
 {
     fn consume<C: Consumer<T>>(self, mut consumer: C) {
         let producer = Rc::new(Producer::new());
@@ -36,48 +29,7 @@ impl<I, T> Stream<T> for IterStream<I>
     }
 }
 
-impl<I, T> StreamRef<T> for IterStreamRef<I>
-    where I: Iterator<Item = T> + 'static
-{
-    fn consume<C: ConsumerRef<T>>(self, mut consumer: C) {
-        let producer = Rc::new(Producer::new());
-
-        consumer.init(producer.clone());
-
-        if producer.is_closed() {
-            return;
-        }
-
-        for i in self.iterator {
-            consumer.emit(&i);
-            if producer.is_closed() {
-                return;
-            }
-        }
-    }
-}
-
-impl<I, T> StreamRefMut<T> for IterStreamRefMut<I>
-    where I: Iterator<Item = T> + 'static
-{
-    fn consume<C: ConsumerRefMut<T>>(self, mut consumer: C) {
-        let producer = Rc::new(Producer::new());
-
-        consumer.init(producer.clone());
-
-        if producer.is_closed() {
-            return;
-        }
-
-        for mut i in self.iterator {
-            consumer.emit(&mut i);
-            if producer.is_closed() {
-                return;
-            }
-        }
-    }
-}
-
+/// Extend the Iterator trait with stream conversion operators
 pub trait ToStream: Iterator {
     /// Convert an iterator to a `Stream`.
     ///
@@ -86,31 +38,16 @@ pub trait ToStream: Iterator {
     /// ```
     /// use asyncplify::*;
     ///
-    /// let mut sum = 0;
-    ///
-    /// (0..5)
+    /// let vec = (0..5)
     ///     .to_stream()
-    ///     .tap(|v| sum += *v)
-    ///     .subscribe();
+    ///     .into_vec();
     ///
-    /// assert!(sum == 10, "sum (0 + 1 + 2 + 3 + 4) = {}", sum);
+    /// assert!(vec == [0, 1, 2, 3, 4], "vec = {:?}", vec);
     /// ``` 
     fn to_stream(self) -> IterStream<Self>
         where Self: Sized
     {
         IterStream { iterator: self }
-    }
-
-    fn to_stream_ref(self) -> IterStreamRef<Self>
-        where Self: Sized
-    {
-        IterStreamRef { iterator: self }
-    }
-
-    fn to_stream_ref_mut(self) -> IterStreamRefMut<Self>
-        where Self: Sized
-    {
-        IterStreamRefMut { iterator: self }
     }
 }
 
